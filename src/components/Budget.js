ebdb90b437d06f27c129e4ccc236f1d2a12a8a70
ggsf-747 <ggsfermanis@gmail.com>
2023-03-22 05:23:23 -0400

    Final Changes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,12 +4,16 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
 	const { budget,dispatch,expenses, currency } = useContext(AppContext);
 
+    const budget_max = 20000
 	const changeBudget = (val)=>{
 		const totalExpenses = expenses.reduce((total, item) => {
 			return (total += item.cost);
 		}, 0);
 
-		
+		if (val > budget_max){
+            alert("Please enter a value not exceeding 20000");
+            return;
+        }
 		if(val<totalExpenses) {
 			alert("You cannot reduce the budget that is already allocated!");
 		} else {
